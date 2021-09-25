f459cd3d4819a8cea60113c7b6988711a6677de1
Ahmed AlShaiba <ahmde031@gmail.com>
2021-09-25 14:14:16 +0400

    fix db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
-  
   def show
     @article = Article.find(params[:id])
   end
-
   def index
     @articles = Article.all
+  end
+  def new
+
+  end
+  def create
+    
   end
 end
