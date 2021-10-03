8a0788f37f88932ff8d7698fe35130bbb8160ea4
Ahmed AlShaiba <ahmde031@gmail.com>
2021-10-03 15:52:42 +0400

    extract away the redundancies

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,54 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
+
   def index
     @articles = Article.all
-    @article = Article.new
   end
+
   def new
     @article = Article.new
   end
+
   def edit
-    @article = Article.find(params[:id])
   end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(white_list)
     if @article.save
-      flash[:notice] = "Article was saved successefully"
+      flash[:notice] = "Article was created successefully"
       redirect_to @article
     else
       render 'new'
     end
   end
+
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(white_list)
       flash[:notice] = "Article was edited successefully"
       redirect_to @article
     else
       render 'edit'
     end
   end
+
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    if @article.destroy
+      flash[:notice] = "Article was deleted successefully"
+    end
     redirect_to articles_path
   end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def white_list
+    params.require(:article).permit(:title, :description)
+  end
+
 end
