1d65f18b5b6983f06978a568ef5d52976f372a35
Ahmed AlShaiba <ahmde031@gmail.com>
2021-11-16 12:24:20 +0400

    add user create action front-end and back-end

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = " Wellcome to Alpha-Blog #{@user.username}, you have signed-up succssfuly."
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
